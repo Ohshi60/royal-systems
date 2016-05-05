b67614c5dd77c47962caa83d0e43a5beaeb4d482
Michael Søby <lakseborgen@gmail.com>
2016-05-05 14:03:54 +0200

    add partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/services'
-  get 'static_pages/contact'
+  root             'static_pages#home'
+  get 'about'    => 'static_pages#about'
+  get 'services'   => 'static_pages#services'
+  get 'contact' => 'static_pages#contact'
 end
